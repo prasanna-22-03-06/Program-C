07c76ee4662832758f7b48b27a4aa190417b0702
prasanna-22-03-06 <you@example.com>
2026-07-03 14:29:19 +0530

    eewdsfwef

---
+++ strong.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,r,f,i;
+    scanf("%d",&a);
+    a=b;
+    while(a!=0){
+        r=a%10;
+        f=1;
+        for(i=1;i<=r;i++){
+            f=f*i;
+        }
+        c=c+f;
+        a=a/10;
+    }
+    printf("%d",c);
+}
