6cea2cf46aae409ff0888b7799b5e5dc410a6eb1
prasanna-22-03-06 <you@example.com>
2026-07-04 11:11:03 +0530

    ef2wfew

---
+++ greatestof3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+   if(a>b){
+    printf("a is grater");
+   }
+   else if(b>c){
+    printf("b is greater"); 
+  }
+    else{
+        printf("c is greater");
+    }
+}
