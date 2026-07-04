40d949eb955ca48359b8165ba82017056dd7df0d
prasanna-22-03-06 <you@example.com>
2026-07-04 11:06:25 +0530

    wefewfw

---
+++ primeornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,count=0,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        if(a%i==0){
+            count++;
+        }
+    }
+    if(count==2){
+        printf("its prime");
+    }
+    else{
+        printf("not prime");
+    }
+}
