ddbce3c38a76fc6ce8fe5410b71808262d9321ac
prasanna-22-03-06 <you@example.com>
2026-07-09 21:43:59 +0530

    weffw

---
+++ lcm.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,b,t,m;
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        t=a;
+    }
+    else{
+        t=b;
+    }
+    while(1){
+        if(a%10==0 && b%10==0){
+            printf("%d",m);
+            break;
+        }
+        m++;
+    }
+}
