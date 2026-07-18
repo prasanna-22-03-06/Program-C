cbd955a39f752d95c48c24bc2f9e7eae3fbcf4d1
prasanna-22-03-06 <you@example.com>
2026-07-18 21:43:46 +0530

    how to print palindrome pyramid

---
+++ palindrome_pyramid.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++)
+    {
+        for(int b=0;b<a-i-1;b++)
+            printf(" ");
+        for(int j=1;j<=i+1;j++)
+            printf("%d",j);
+        for(int j=i;j>=1;j--)
+            printf("%d",j);
+        printf("\n");
+    }
+}
